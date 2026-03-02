c5eba809b96cb4e53c993e9bbb18c67f5e39939f
wateralsie <minee1913@gmail.com>
2026-03-02 15:15:23 +0900

    [chore] springdoc 버전 2.8.15로 업데이트
    Swagger 문서가 실행되지 않는 이슈 해결

--- src/main/java/com/likelion/chatbot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/likelion/chatbot/exception/GlobalExceptionHandler.java
@@ -5,10 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(annotations = {RestController.class})
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ChatbotException.class)
